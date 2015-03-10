dcfe8eb320aab386b52783dfe1f4cefa53a288e4
alexlittle <alex@alexlittle.net>
2015-03-10 22:21:28 +0100

    refactoring

--- examples/index.php
+++ examples/index.php
@@ -3,19 +3,8 @@
 include_once '../mpower-api/api.php';
 
  if (isset($_POST['q'])){
- 	
- 	$config = new mPowerConfig('http://localhost:8000/','demo','39b4043c69b8db27ddba761ba82479d00c8ccbb1');
- 	$curl = curl_init();
- 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1 );
- 	$temp_url = "http://mpowering.digital-campus.org/api/v1/resource/search/";
- 	$temp_url .= "?format=json";
- 	$temp_url .= "&username=demo";
- 	$temp_url .= "&api_key=39b4043c69b8db27ddba761ba82479d00c8ccbb1";
- 	$temp_url .= "&q=". $_POST['q'];
- 	curl_setopt($curl, CURLOPT_URL, $temp_url );
- 	curl_setopt($curl, CURLOPT_HTTPGET, 1 );
- 	$data = curl_exec($curl);
- 	$results = json_decode($data);
+	$api = new mPowerAPI('http://localhost:8000','demo','39b4043c69b8db27ddba761ba82479d00c8ccbb1');
+	$results =  $api->search($_POST['q']);
  }
 ?>
 <html>
--- mpower-api/api.php
+++ mpower-api/api.php
@@ -2,29 +2,68 @@
 
 class mPowerAPI {
 	
-	function search($config, $query){
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1 );
-		$temp_url = $config->base_url."api/v1/resource/search/";
-		$temp_url .= "?format=json";
-		$temp_url .= "&username=".$config->username;
-		$temp_url .= "&api_key=39b4043c69b8db27ddba761ba82479d00c8ccbb1";
-		$temp_url .= "&q=". $_POST['q'];
-		curl_setopt($curl, CURLOPT_URL, $temp_url );
-		curl_setopt($curl, CURLOPT_HTTPGET, 1 );
-		$data = curl_exec($curl);
-		return json_decode($data);
-	}
-	
-	
-	
-}
-
-class mPowerConfig {
 	private $base_url;
 	private $username;
 	private $api_key;
 	
+	public function __construct($base_url, $username, $api_key){
+		
+		if (substr($base_url, -strlen('/'))==='/'){
+			$this->base_url = $base_url;
+		} else {
+			$this->base_url = $base_url."/";
+		}
+		$this->username = $username;
+		$this->api_key = $api_key;
+	}
+	
+	function search($query){
+		$results = $this->exec('resource/search',['q'=>$query],'get');
+		return $results;
+	}
+	
+	
+	# add resource
+	# add basic info
+	# add image
+	# add files
+	# add urls
+	# organsation
+	# core tags
+	# other tags
+	
+	
+	
+	# edit/update resource
+	
+	
+	
+	private function exec($object, $data_array, $type='post'){
+	
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1 );
+
+		$temp_url = $this->base_url."api/v1/".$object."/";
+		$temp_url .= "?format=json";
+		$temp_url .= "&username=".$this->username;
+		$temp_url .= "&api_key=".$this->api_key;
+		
+		if($type == 'post'){
+			$json = json_encode($data_array);
+			curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
+			curl_setopt($curl, CURLOPT_POST,1);
+			curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($json) ));
+		} else {
+			curl_setopt($curl, CURLOPT_HTTPGET, 1 );
+			$temp_url .= "&".http_build_query($data_array);
+		}
+		curl_setopt($curl, CURLOPT_URL, $temp_url );
+		$data = curl_exec($curl);
+		$json = json_decode($data);
+		$http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+		return $json;
+			
+	}
 	
 }
 
