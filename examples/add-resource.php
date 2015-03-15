0595f0cdac49c6f44aa5a5c03f71c161caf8f141
alexlittle <alex@alexlittle.net>
2015-03-15 15:34:01 +0100

    add comments

--- examples/add-resource.php
+++ examples/add-resource.php
@@ -4,18 +4,23 @@ include_once '../mpower-api/api.php';
 
 $api = new mPowerAPI('http://localhost:8000','demo','39b4043c69b8db27ddba761ba82479d00c8ccbb1');
 
+// Core resource info
 $resource = new mPowerResource();
 $resource->title = "another test ".date('Y-m-d H:i:s');
 $resource->description = "description of something or other";
 $resource->image = "/home/alex/temp/image.jpg";
+
+// Append tags
 $resource->tags = ['Global Health Media Project', 'Africa', 'Laptop', 'smartphone'];
 
+// Append URLs
 $url = new mPowerURL();
 $url->url = "http://duckduckgo.com";
 $url->title = "Duck Duck Go";
 $url->description = "search engine";
 array_push($resource->urls,$url);
 
+// Append files
 $file1 = new mPowerFile();
 $file1->file = "/home/alex/temp/hews1.csv";
 $file1->title = "HEWs";
@@ -28,6 +33,7 @@ $file2->title = "Tutors";
 $file2->description = "all tutors";
 array_push($resource->files,$file2);
 
+// Actually add the resource
 $api->add_resource($resource);
 		
 ?>
