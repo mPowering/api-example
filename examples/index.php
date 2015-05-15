d3739bcc53adc6f97639518c1b156be02421703b
alexlittle <alex@alexlittle.net>
2015-05-15 11:00:15 -0400

    exclude add resource option...

--- examples/index.php
+++ examples/index.php
@@ -7,7 +7,7 @@
 <h2>Demo of ORB API</h2>
 <ul>
 <li><a href="search.php">Search</a></li>
-<li><a href="add-resource.php">Add Resource</a></li>
+<!-- li><a href="add-resource.php">Add Resource</a></li -->
 </ul>
 </body>
 </html>
